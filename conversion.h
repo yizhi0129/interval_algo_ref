cf2b2ff277545bdac24738d27a71979aa9c38a3e
César Mathéus <ces4r.m4theus@gmail.com>
2021-08-27 09:24:27 +0000

    Replace conversion.h

--- conversion.h
+++ conversion.h
@@ -5,9 +5,30 @@
 #include"toolBox.h"
 
 
-INF_SUP FpInt2InfSup(FP_INT x);
+static inline INF_SUP FpInt2InfSup(FP_INT x){
+	union ieee754_double f;
+	f.d = (double)(x);
+	double ulss = uls(x);
+	INF_SUP res;
+	res.inf = f.d-ulss;
+	res.sup = f.d+ulss;
+	return res;
+}
 
-MID_RAD InfSup2MidRad(INF_SUP x);
+static inline MID_RAD InfSup2MidRad(INF_SUP x){
+	union ieee754_double mid;
+	union ieee754_double rad;
+
+	mid.d = x.sup+x.inf;
+	mid.ieee.exponent = mid.ieee.exponent-1;
+	rad.d = x.sup-x.inf;
+	rad.ieee.exponent = rad.ieee.exponent-1;
+
+	MID_RAD res;
+	res.mid = mid.d;
+	res.rad = rad.d;
+	return res;
+}
 
 FP_INT intval2FpInt(double center, double rad);
 
